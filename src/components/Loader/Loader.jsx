2831429abba0f953a1a900070cbc38ea5de8c1f3
InnaShavarovska <innashavarovska@gmail.com>
2023-05-15 19:47:50 +0300

    Hearts

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,18 +1,15 @@
-import { Oval } from 'react-loader-spinner';
+import { Hearts } from 'react-loader-spinner';
 export const Loader = () => {
   return (
     <div>
-      <Oval
+      <Hearts
         height={80}
         width={80}
-        color="#3f51b5"
+        color="#4fa94d"
+        ariaLabel="hearts-loading"
         wrapperStyle={{}}
         wrapperClass=""
         visible={true}
-        ariaLabel="oval-loading"
-        secondaryColor="#3f51b5"
-        strokeWidth={2}
-        strokeWidthSecondary={2}
       />
     </div>
   );
